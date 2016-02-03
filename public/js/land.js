68a08011b8e71d035038a14f2b0da1487aa0f63d
Luiggi Anguiano <luiggi@luiggianguiano.com.br>
2016-02-03 20:03:08 -0200

    land2

---
+++ public/js/land.js
@@ -0,0 +1,11 @@
+(function() {
+	var windowHeight = $(window).height();
+	var header = $('header');
+	var menu = header.children('.menu');
+
+	header.height(windowHeight + menu.height());
+	header.children('.container').height(windowHeight);
+	menu.attr('data-offset-top', windowHeight);
+	
+	$(".public-logo").parallax("50%", 0.4);
+})();
